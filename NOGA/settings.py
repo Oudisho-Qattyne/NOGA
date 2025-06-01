2fb552eb209be4875eda27d011bc78bb63195e48
Naya-zidan <naya4zidan@gmail.com>
2025-06-01 22:36:22 +0300

    CORS ORIGIN ALLOW ALL

--- NOGA/settings.py
+++ NOGA/settings.py
@@ -146,7 +146,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
