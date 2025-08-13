2b357fd721e35be789ceda49266f6520fed96f11
Oudisho-Qattyne <oudi2003@gmail.com>
2025-08-13 15:20:12 +0300

    fixed products filters

--- mobile/filters.py
+++ mobile/filters.py
@@ -7,8 +7,7 @@ class MobileProductFilter(django_filters.FilterSet):
     attribute = django_filters.CharFilter(field_name='variant__options__attribute__attribute', lookup_expr='icontains')
     category = django_filters.CharFilter(field_name='category__category', lookup_expr='icontains')
     product = django_filters.CharFilter(field_name='product_name', lookup_expr='icontains')
-    selling_price =  django_filters.RangeFilter(field_name='variant__selling_price')
-    wholesale_price = django_filters.RangeFilter(field_name='variant__wholesale_price')
+    price =  django_filters.RangeFilter(field_name='variant__selling_price')
     quantity = django_filters.RangeFilter(field_name='variant__quantity')
     sku:django_filters.CharFilter(field_name='variant__sku', lookup_expr='icontains')
     class Meta:
