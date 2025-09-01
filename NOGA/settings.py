85ef6b43bbf53263f0245415e2716de46e2488ae
Oudisho-Qattyne <oudi2003@gmail.com>
2025-09-01 18:43:21 +0300

    trying to cut unwanted libraries to free some space 4

--- NOGA/settings.py
+++ NOGA/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'NOGA.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }
+# } 
 
 DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
